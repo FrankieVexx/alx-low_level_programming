4b71cd592393a542929c60dc4a8b9c7788afc06c
Francis Onyach <franblog7@gmail.com>
2022-09-09 12:06:46 -0700

    print alphabets 2

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabets in lowercase followed by upper case
+ * new line is placed
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
