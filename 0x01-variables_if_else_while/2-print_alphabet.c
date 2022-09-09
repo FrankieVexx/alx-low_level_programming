2681f6b5f941cbe4e6d6edbf64b16299bfe3c8f3
Francis Onyach <franblog7@gmail.com>
2022-09-09 11:34:38 -0700

    writing alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - reads characters in the alphabet
+ * print results in the correct order in lower case
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ch;
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
