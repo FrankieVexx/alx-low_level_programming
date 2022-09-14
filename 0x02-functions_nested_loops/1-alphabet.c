7f3631d820a64ba79e125d6ad5ea61791e78cbd6
Francis Onyach <franblog7@gmail.com>
2022-09-14 05:39:15 -0700

    1-alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,10 +7,10 @@
  */
 void print_alphabet(void)
 {
-	char letter;
+	char c;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
-		_putchar(letter);
+	for (c = 'a'; c <= 'z'; c++)
+		_putchar(c);
 
-	putchar('\n');
+	_putchar('\n');
 }
