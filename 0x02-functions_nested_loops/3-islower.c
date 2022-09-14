5a7c9f32a9cf729946f74da868cde1975746d266
Francis Onyach <franblog7@gmail.com>
2022-09-14 06:20:42 -0700

    lower case indentifier

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _slower - enrty point, int ipnut type and int arg
+ *
+ * @c: checks for lowercase character and returns different int
+ *
+ * Return 1 or 0
+ */
+int _islower(int c)
+{
+	if (c > 96 && c <= 122)
+	{
+		return (1)
+	}
+	else
+		return (0);
+}
