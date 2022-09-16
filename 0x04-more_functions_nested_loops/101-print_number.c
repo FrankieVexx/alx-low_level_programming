a1c54b21e5a263344a986e4b1d1f5ec3c9eb8d6e
Francis Onyach <franblog7@gmail.com>
2022-09-16 09:19:08 -0700

    print new numbers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,18 +6,22 @@
  */
 void print_number(int n)
 {
-	unsigned int num = n;
+	unsigned int n1;
 
 	if (n < 0)
 	{
+		n1 = -n;
 		_putchar('-');
-		num = -num;
-	}
-
-	if ((num / 10) > 0)
+	} else
 	{
-		print_number(num / 10);
-
-	_putchar((num % 10) + '0');
+		n1 = n;
 	}
+
+	if (n1 / 10)
+	{
+		print_number(n1 / 10);
+	}
+
+	_putchar((n1 % 10) + '0');
 }
+
