de18bd3a01569967fb452c5664eb7b0ac97c0b2f
Francis Onyach <franblog7@gmail.com>
2022-09-22 03:32:46 -0700

    comparing 2 strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,21 +4,15 @@
  * _strcmp - compares two strings
  * @s1: The first string
  * @s2: The secong string
- * Return: 1 if true, 0 otherwise.
+ * Return: The results of the comparison between the 2 strings
  */
 int _strcmp(char *s1, char *s2)
 {
-	int equal = 0;
-
-	while (*s1)
+	while (*s1 && *s2 && *s1 == *s2)
 	{
-		if (*s != *s2)
-		{
-			equal = ((int)*s1 - 48) - ((int)*s2 - 48);
-			break;
-		}
 		s1++;
 		s2++;
 	}
-	return (equal);
+
+	return (*s1 - *s2);
 }
