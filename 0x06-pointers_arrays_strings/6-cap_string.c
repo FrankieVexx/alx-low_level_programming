f9477f3c0edffd2988e7b0c597716281af37f031
Francis Onyach <franblog7@gmail.com>
2022-09-22 12:24:05 -0700

    leet created

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,33 +2,18 @@
 
 /**
  * *cap_string - capitalizes all the words in a string
- * @s: the string to be modified
+ * @str: the string to be modified
  *
- * Return: the rsulting string
+ * Return: A pointer to the string changed
  */
 char *cap_string(char *s)
 {
-	int 1, j;
+	int index = 0;
 
-	char spe[13] = {' ', '\t', '\n', ',', ';', '.',
-		'!', '?', '"', '(', ')', '{', '}'};
-
-	for (i = 0; s[i] != '\0'; i++)
+	while (str[index])
 	{
-		if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
-			s[i] -= 32;
+		while (!(str[index] >= 'a' && str[index] <= 'z'))
+			index++;
 
-		for (j = 0; j < 13; j++)
-		{
-			if (s[i] == spe[j])
-			{
-				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
-				{
-					s[i + 1] -= 32;
-				}
-			}
-		}
-	}
+		if (str[index - 1] == ' ' ||
 
-	return (s);
-}
