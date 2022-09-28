99ab7ae317d74e76f77380f9ceb41d19b1fb3301
Francis Onyach <franblog7@gmail.com>
2022-09-27 18:34:39 -0700

    changes to to 3-strspn.c.

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -4,29 +4,27 @@
  * _strspn - gets the length of a prefix substring
  * @s: the initial segment
  * @accept: the accepted bytes of data
- * Return: the unsigned int @s
+ * Return: the number of accepted bytes
  */
 
 unsigned int _strspn(char *s, char *accept)
 {
 
-	int i, j, sim = 0;
+	unsigned int i, j, bool;
 
-	for (i = 0; s[i] > '\0'; i++)
+	for (i = 0; *(s + i) != '\0'; i++)
 	{
-		for (j = 0; accept[j] > '\0'; j++)
+		bool = 1;
+		for (j = 0; *(accept + j) != '\0'; j++)
 		{
-			if  (s[i] == accept[j])
+			if (*(s + 1) == *(accept + j))
 			{
-				sim++;
+				bool = 0;
 				break;
 			}
 		}
-		if (accept[j] == '\0')
-		{
+		if (bool == 1)
 			break;
-		}
 	}
-
-	return (sim);
+	return (i);
 }
