654a2b1d2e582b072c965066828cf06c3dd387a2
Francis Onyach <franblog7@gmail.com>
2022-09-30 07:42:03 -0700

    myname is

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the name of the program
+ * @argc: the number of arguments in the program
+ * @argv: the nummber of arrays of string the argument
+ * Return: Always 0
+ */
+
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+		return (0);
+}
--- 0x0A-argc_argv/README.md
+++
@@ -1,4 +0,0 @@
-Argc and Argv Concepts
-
-Argc - the count(number of arguments given)
-Argv - the vector( number of array of strings containing the commands in a pogram
