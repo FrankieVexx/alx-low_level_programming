cabe55cbd4e97cebc934f20c41d9868788ac097d
Francis Onyach <franblog7@gmail.com>
2022-09-30 06:55:09 -0700

    Argc & Argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments passed to it
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("argc = %d\n", argc);
+	return 0;
+}
