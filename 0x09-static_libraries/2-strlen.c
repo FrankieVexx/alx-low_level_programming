90a487984081bcb2a6e1f7046bc30a3049d582c5
Francis Onyach <franblog7@gmail.com>
2022-09-30 05:28:54 -0700

    libmy created

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _strlen - prints the length of a string
+ * @s: the string
+ *
+ * Return: length of a string
+ */
+
+int _strlen(char *s)
+{}
+
---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @n: the integer
+ * Return: Integer
+ */
+
+int  _abs(int n)
+{}
+
