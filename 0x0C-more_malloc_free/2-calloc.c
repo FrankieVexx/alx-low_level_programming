349cd15ba565d676a055ae31c9c0314fce91710d
Francis Onyach <franblog7@gmail.com>
2022-10-06 10:47:19 -0700

    correction to calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -15,22 +15,22 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *mem;
-	char *filler;
-	unsigned int index;
+	int i = 0, l = 0;
+	char *ptr;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	mem = malloc(size * nmemb);
+	l = nmemb * size;
+	p = malloc(l);
 
-	if (mem == NULL)
+	if (p == NULL)
 		return (NULL);
 
-	filler = mem;
-
-	for (index = 0, index < (size * nmemb); index++)
-		filler[index] = '\0';
-
-	return (mem);
+	while (i < l)
+	{
+		p[i] = 0;
+		i++;
+	}
+	return (p);
 }
