987c9f7d5897713bede7338fbfbfd2840ab4909b
Francis Onyach <franblog7@gmail.com>
2022-10-06 19:16:45 -0700

    string concat correction

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -17,32 +17,26 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *concat;
-	unsigned int len = n, i;
+	char *s;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-	if (s1 == NULL)
-		s1 = "";
+	while (s1 && s1[len1])
+		len1++;
+	while (s2 && s2[len2])
+		len2++;
 
-	if (s2 == NULL)
-		s2 = "";
+	if (n < len2)
+		s = malloc(sizeof(char) * (len1 + n + 1));
+	else
+		s = malloc(sizeof(char) * (len1 + len2 + 1));
 
-	for (i = 0; s1[i]; i++)
-		len++;
-
-	concat = malloc(sizeof(char) * (len + 1));
-
-	if (concat == NULL)
+	if (!s)
 		return (NULL);
 
-	len = 0;
+	while (n < len && i < (len1 + n))
+		s[i++] = s2[j++];
 
-	for (i = 0; s1[i]; i++)
-		concat[len++] = s1[i];
+	s[i] = '\0';
 
-	for (i = 0; s2[i] && i < n; i++)
-		concat[len++] = s2[i];
-
-	concat[len] = '\0';
-
-	return (concat);
+	return (s);
 }
