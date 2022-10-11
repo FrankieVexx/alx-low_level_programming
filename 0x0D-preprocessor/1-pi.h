a4977a639582673d84373328c68d8965f17d2c5e
Francis Onyach <franblog7@gmail.com>
2022-10-11 05:30:09 -0700

    Created file 1-pi.h

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,12 @@
+#ifndef PI_H
+#define PI_H
+
+/**
+ * file: PI.H
+ * description: header file that defines macro PI for an abbreviation
+ * of token 3.14159265359
+ */
+
+#define PI 3.14159265359
+
+#endif
