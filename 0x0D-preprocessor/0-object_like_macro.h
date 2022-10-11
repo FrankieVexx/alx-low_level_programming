6bfbec1ce2059792db9d57350b82b74ff7236fe3
Francis Onyach <franblog7@gmail.com>
2022-10-11 04:20:48 -0700

    object like file

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+/*
+ * file: 0-object_like_macro.h
+ * Description: A header file that defines macro SIZE as an abbreviation
+ * of 1024
+ */
+
+#define SIZE 1024
+
+#endif
