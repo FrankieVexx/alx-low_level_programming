e0823041498d7b52d1f9d0a43cfc809ece82302e
Francis Onyach <franblog7@gmail.com>
2022-10-11 07:41:34 -0700

    macros for 4-sum.h

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,12 @@
+#ifndef SUM_H
+#define SUM_H
+
+/*
+ * file name: 4-sum.h
+ * Description: A header file that defines function-like macro SUM(x, y) that
+ * computes the sum of two numbers x and y
+ */
+
+#define SUM(x, y) ((x) + (y))
+
+#endif
