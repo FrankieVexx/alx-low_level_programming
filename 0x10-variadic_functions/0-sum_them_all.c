4d8f9f643e4a39ed16bcd0d50a8550e6aceaf42f
Francis Onyach <franblog7@gmail.com>
2022-10-14 07:03:15 -0700

    correction to sum them all

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -13,12 +13,12 @@ int sum_them_all(const unsigned int n, ...)
 	va_list ap;
 	int i, sum;
 
-	va_start (ap, n);
+	va_start(ap, n);
 
 	sum = 0;
 	for (i = 0; i < n; i++)
 		sum += va_arg(ap, unsigned int);
 
-	va_end (ap);
+	va_end(ap);
 	return (sum);
 }
