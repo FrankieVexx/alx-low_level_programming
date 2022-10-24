871ad786f0f2d79297a288d4328547d12e586d67
Francis Onyach <franblog7@gmail.com>
2022-10-24 06:23:54 -0700

    lists file and task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all elements of a list_t list
+ * @h: the singly linked list
+ * Return: the number of elements in the list
+ */
+
+size_t ptint_list(const list_t *h)
+{
+	size_t nelms;
+
+	nelms = 0;
+	while (h != NULL)
+	{
+		if (h->str ==  NULL)
+			printf("[%d] %s\n", 0, "(nil)");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		nelms++;
+	}
+	return (nelms);
+}
