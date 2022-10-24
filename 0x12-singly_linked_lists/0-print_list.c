6781f6b299d7de129c1ccc54eed44351db0c842e
Francis Onyach <franblog7@gmail.com>
2022-10-24 06:34:15 -0700

    correction to task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -17,8 +17,9 @@ size_t print_list(const list_t *h)
 			printf("[%d] %s\n", 0, "(nil)");
 		else
 			printf("[%d] %s\n", h->len, h->str);
-		h = h->next;
+
 		nelms++;
+		h = h->next;
 	}
 	return (nelms);
 }
