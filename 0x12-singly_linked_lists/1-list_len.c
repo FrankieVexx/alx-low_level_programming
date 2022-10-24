49c24c2e34ff7ea79ed8042ecfb8ef0d4749ad61
Francis Onyach <franblog7@gmail.com>
2022-10-24 09:05:13 -0700

    correction to 1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * list-len - finds the number of elements in a linked list_t list
+ * list_len - finds the number of elements in a linked list_t list
  * @h: the linked list_t list
  *
  * Return: the number of elements in list h
@@ -10,7 +10,7 @@ size_t list_len(const list_t *h)
 {
 	size_t elms = 0;
 
-	while(h)
+	while (h)
 	{
 		elms++;
 		h = h->next;
