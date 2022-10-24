f9da11d62af98e2656e2b45677a79ad098032322
Francis Onyach <franblog7@gmail.com>
2022-10-24 06:55:52 -0700

    task 1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * list-len - finds the number of elements in a linked list_t list
+ * @h: the linked list_t list
+ *
+ * Return: the number of elements in list h
+ */
+size_t list_len(const list_t *h)
+{
+	size_t elms = 0;
+
+	while(h)
+	{
+		elms++;
+		h = h->next;
+	}
+
+	return (elms);
+}
