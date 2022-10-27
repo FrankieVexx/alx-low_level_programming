e474d24bd8c7e739b49cef3718d5c19a4cb4c6b2
Francis Onyach <franblog7@gmail.com>
2022-10-27 01:44:31 -0700

    corrections

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -10,7 +10,7 @@
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	listint_t *clr, copy = *head;
+	listint_t *clr, *copy = *head;
 	unsigned int node;
 
 	if (copy == NULL)
