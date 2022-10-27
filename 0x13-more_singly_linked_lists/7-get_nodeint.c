f56729d77e62b8140b960b908d35beb877ada2bc
Francis Onyach <franblog7@gmail.com>
2022-10-27 00:41:58 -0700

    find node

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - finds a node from a linked list listint_t
+ * @head: a pointer to the head of the lisint_t
+ * @index: the position of the node to locate from
+ * the indices
+ *
+ * Return: The found node or NULL if it does not exist
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int node;
+
+	for (node = 0; node < index; node++)
+	{
+		if (head == NULL)
+			return (NULL);
+
+		head = gead->next;
+	}
+
+	return (head);
+}
