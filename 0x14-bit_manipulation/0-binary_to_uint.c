32919468c55594bed94fb8b01dcab5dcfe2e9c76
Francis Onyach <franblog7@gmail.com>
2022-10-31 09:09:17 -0700

    corrections 2

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,7 +2,7 @@
 
 /**
  * binary_to_uint - converts a binary number to an unsigned int
- * @b: a pointer to a string characters 0 and 1 
+ * @b: a pointer to a string characters 0 and 1
  *
  * Return: The converted number or 0 if the character is not 0 or 1
  */
