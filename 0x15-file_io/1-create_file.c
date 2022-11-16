2821b532d24a787502c8375a6a34021e705855a5
Francis Onyach <franblog7@gmail.com>
2022-11-16 07:18:54 -0800

    1 create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,37 +1,33 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * create_file- creates a file
- * @filename: name of the file to create
- * @text_content: NULL terminated string writtento the file
+ * create_file - Creates a file.
+ * @filename: A pointer to the name of the file to create.
+ * @text_content: A pointer to a string to write to the file.
  *
- * Return: 1 on success, -1 on failure or if file is NULL
+ * Return: If the function fails - -1.
+ *         Otherwise - 1.
  */
 int create_file(const char *filename, char *text_content)
 {
-	int _file, _write, len = 0;
+	int o, w, len = 0;
 
-	if (!filename)
-		return (-1);
-
-	_file = open(filename, O_WRONLY, | O_CREAT | O_TRUNC, 0600);
-	if (file == -1)
+	if (filename == NULL)
 		return (-1);
 
 	if (text_content != NULL)
 	{
-		while (text_content[len])
+		for (len = 0; text_content[len];)
 			len++;
 	}
 
-	_write = write(_file, text_content, len);
-	if (_write == -1)
-	{
-		close(_file);
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(o, text_content, len);
+
+	if (o == -1 || w == -1)
 		return (-1);
-	}
 
-	close(_file);
+	close(o);
+
 	return (1);
-
 }
