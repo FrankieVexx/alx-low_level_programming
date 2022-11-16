bb169de99e499e07920964903033264764e65848
Francis Onyach <franblog7@gmail.com>
2022-11-16 07:52:42 -0800

    task 6 correction

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,19 +1,35 @@
 #include "main.h"
 
 /**
- * *cap_string - capitalizes all the words in a string
- * @str: the string to be modified
+ * cap_string - capitalizes everey word of a string
+ * @s: string to modify
  *
- * Return: A pointer to the string changed
+ * Return: the resulting string
  */
 char *cap_string(char *s)
 {
-	int index = 0;
+	int i, j;
 
-	while (str[index])
+	char spe[13] = {' ', '\t', '\n', ',', ';', '.',
+		'!', '?', '"', '(', ')', '{', '}'};
+
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		while (!(str[index] >= 'a' && str[index] <= 'z'))
-			index++;
+		if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
+			s[i] -= 32;
 
-		if (str[index - 1] == ' ' ||
+		for (j = 0; j < 13; j++)
+		{
+			if (s[i] == spe[j])
+			{
+				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+				{
+					s[i + 1] -= 32;
+				}
+			}
+		}
+	}
+
+	return (s);
+}
 
